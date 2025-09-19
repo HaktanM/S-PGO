19bef0562910a5b0bc5d7e66fe220dda022ccfc2
HaktanM <haktanyalcinn@gmail.com>
2025-09-19 19:24:30 +0300

    Clean the code

---
+++ build_gp/CMakeFiles/Solver.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/haktanito/workspace/S-PGO/src/LieUtils/SE3.cu" "CMakeFiles/Solver.dir/src/LieUtils/SE3.cu.o" "gcc" "CMakeFiles/Solver.dir/src/LieUtils/SE3.cu.o.d"
+  "/home/haktanito/workspace/S-PGO/src/LieUtils/SO3.cu" "CMakeFiles/Solver.dir/src/LieUtils/SO3.cu.o" "gcc" "CMakeFiles/Solver.dir/src/LieUtils/SO3.cu.o.d"
+  "/home/haktanito/workspace/S-PGO/src/LieUtils/utils.cu" "CMakeFiles/Solver.dir/src/LieUtils/utils.cu.o" "gcc" "CMakeFiles/Solver.dir/src/LieUtils/utils.cu.o.d"
+  "/home/haktanito/workspace/S-PGO/src/Optimization/LMutils.cu" "CMakeFiles/Solver.dir/src/Optimization/LMutils.cu.o" "gcc" "CMakeFiles/Solver.dir/src/Optimization/LMutils.cu.o.d"
+  "/home/haktanito/workspace/S-PGO/src/Optimization/Solver_cuda.cu" "CMakeFiles/Solver.dir/src/Optimization/Solver_cuda.cu.o" "gcc" "CMakeFiles/Solver.dir/src/Optimization/Solver_cuda.cu.o.d"
+  "/home/haktanito/workspace/S-PGO/src/Optimization/linear_solvers.cu" "CMakeFiles/Solver.dir/src/Optimization/linear_solvers.cu.o" "gcc" "CMakeFiles/Solver.dir/src/Optimization/linear_solvers.cu.o.d"
+  "/home/haktanito/workspace/S-PGO/build_gp/Solver.cpp" "CMakeFiles/Solver.dir/Solver.cpp.o" "gcc" "CMakeFiles/Solver.dir/Solver.cpp.o.d"
+  "/home/haktanito/workspace/S-PGO/src/Optimization/Solver.cpp" "CMakeFiles/Solver.dir/src/Optimization/Solver.cpp.o" "gcc" "CMakeFiles/Solver.dir/src/Optimization/Solver.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_gp/CMakeFiles/Solver.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Solver.dir/Solver.cpp.o"
+  "CMakeFiles/Solver.dir/Solver.cpp.o.d"
+  "CMakeFiles/Solver.dir/cmake_device_link.o"
+  "CMakeFiles/Solver.dir/src/LieUtils/SE3.cu.o"
+  "CMakeFiles/Solver.dir/src/LieUtils/SE3.cu.o.d"
+  "CMakeFiles/Solver.dir/src/LieUtils/SO3.cu.o"
+  "CMakeFiles/Solver.dir/src/LieUtils/SO3.cu.o.d"
+  "CMakeFiles/Solver.dir/src/LieUtils/utils.cu.o"
+  "CMakeFiles/Solver.dir/src/LieUtils/utils.cu.o.d"
+  "CMakeFiles/Solver.dir/src/Optimization/LMutils.cu.o"
+  "CMakeFiles/Solver.dir/src/Optimization/LMutils.cu.o.d"
+  "CMakeFiles/Solver.dir/src/Optimization/Solver.cpp.o"
+  "CMakeFiles/Solver.dir/src/Optimization/Solver.cpp.o.d"
+  "CMakeFiles/Solver.dir/src/Optimization/Solver_cuda.cu.o"
+  "CMakeFiles/Solver.dir/src/Optimization/Solver_cuda.cu.o.d"
+  "CMakeFiles/Solver.dir/src/Optimization/linear_solvers.cu.o"
+  "CMakeFiles/Solver.dir/src/Optimization/linear_solvers.cu.o.d"
+  "Solver.cpp"
+  "Solver.pdb"
+  "Solver.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/Solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
